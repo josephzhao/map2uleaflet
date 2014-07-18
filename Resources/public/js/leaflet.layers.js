5e0f8c7638af39eec3953e218bb7aefe29ca3c7a
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2014-07-18 16:49:49 -0400

    remove mapinfo files upload map preview button

--- Resources/public/js/leaflet.layers.js
+++ Resources/public/js/leaflet.layers.js
@@ -1233,7 +1233,7 @@ L.MAP2U.layers = function(options) {
                                 var shapefilename = $('.sonata-bc #shapefile_select_list option:selected').map(function() {
                                     return  this.text;
                                 });
-                                if (shapefilename !== '' && shapefilename[0] !== undefined && geojson_shapefile.options.name === shapefilename[0].toLowerCase())
+                                if (shapefilename !== '' && shapefilename[0] !== undefined && geojson_shapefile.options.filename === shapefilename[0].toLowerCase())
                                 {
 
                                     if ($('#geometries_selected').length > 0)
