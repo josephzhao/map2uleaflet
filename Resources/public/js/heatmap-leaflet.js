b1d69d8b57d879cdc4586f4244e4fc3b769a018f
Joseph Zhao <zhaoshuilin2004@yahoo.ca>
2014-07-28 10:09:25 -0400

    add leaflet heatmap

--- Resources/public/js/heatmap-leaflet.js
+++ Resources/public/js/heatmap-leaflet.js
@@ -10,21 +10,20 @@
  *  - QuadTree: https://github.com/jsmarkus/ExamplesByMesh/tree/master/JavaScript/QuadTree
  */
 
- L.TileLayer.HeatMap = L.TileLayer.Canvas.extend({
-	options: {
+L.TileLayer.HeatMap = L.TileLayer.Canvas.extend({
+    options: {
         debug: false,
-        opacity: 0.9,  // opactity is between 0 and 1, not in percent
+        opacity: 0.9, // opactity is between 0 and 1, not in percent
         radius: {
             value: 20,
             absolute: false  // true: radius in meters, false: radius in pixels
         }
-	},
-
-	initialize: function (options, data) {
+    },
+    initialize: function(options, data) {
         var self = this;
         L.Util.setOptions(this, options);
 
-        this.drawTile = function (tile, tilePoint, zoom) {
+        this.drawTile = function(tile, tilePoint, zoom) {
             var ctx = {
                 canvas: tile,
                 tilePoint: tilePoint,
@@ -36,7 +35,7 @@
             }
             this._draw(ctx);
         };
-                var legend = function legend(config) {
+        var legend = function legend(config) {
             this.config = config;
 
             var _ = {
@@ -61,11 +60,11 @@
                         position = config.position,
                         offset = config.offset || 10,
                         gconfig = config.gradient,
-                        width=config.width|| 320,
+                        width = config.width || 320,
                         labelsEl = document.createElement("ul"),
                         labelsHtml = "",
                         grad, element, gradient, positionCss = "";
-                me.set("gradient_width", width-22);
+                me.set("gradient_width", width - 22);
                 me.processGradientObject();
 
                 // Positioning
@@ -85,7 +84,7 @@
                 }
 
                 element = document.createElement("div");
-                element.style.cssText = "border-radius:5px;position:absolute;" + positionCss + "font-family:Helvetica; width:"+width+"px;z-index:10000000000; background:rgba(255,255,255,1);padding:10px;border:1px solid black;margin:0;";
+                element.style.cssText = "border-radius:5px;position:absolute;" + positionCss + "font-family:Helvetica; width:" + width + "px;z-index:10000000000; background:rgba(255,255,255,1);padding:10px;border:1px solid black;margin:0;";
                 element.innerHTML = "<h3 style='padding:0;margin:0;text-align:center;font-size:16px;'>" + title + "</h3>";
                 // create gradient in canvas
                 labelsEl.style.cssText = "position:relative;font-size:12px;display:block;list-style:none;list-style-type:none;margin:0;height:15px;";
@@ -93,14 +92,14 @@
 
                 // create gradient element
                 gradient = document.createElement("div");
-                gradient.style.cssText = ["position:relative;display:block;width:"+(width-22)+"px;height:15px;border-bottom:1px solid black; background-image:url(", me.createGradientImage(), ");"].join("");
+                gradient.style.cssText = ["position:relative;display:block;width:" + (width - 22) + "px;height:15px;border-bottom:1px solid black; background-image:url(", me.createGradientImage(), ");"].join("");
 
                 element.appendChild(labelsEl);
                 element.appendChild(gradient);
 
                 me.set("element", element);
                 me.set("labelsEl", labelsEl);
-               
+
                 me.update(1);
             },
             processGradientObject: function() {
@@ -129,7 +128,7 @@
                         ctx = canvas.getContext("2d"),
                         grad;
                 // the gradient in the legend including the ticks will be 256x15px
-                canvas.width = me.get("gradient_width")||"256";
+                canvas.width = me.get("gradient_width") || "256";
                 canvas.height = "15";
 
                 grad = ctx.createLinearGradient(0, 5, canvas.width, 10);
@@ -147,9 +146,9 @@
                     ctx.moveTo(((1 / (length - 1) * i * canvas.width) >> 0) + .5, 0);
                     ctx.lineTo(((1 / (length - 1) * i * canvas.width) >> 0) + .5, (i == 0) ? 15 : 5);
                 }
-                ctx.moveTo(canvas.width-0.5, 0);
-                ctx.lineTo(canvas.width-0.5, 15);
-                ctx.moveTo(canvas.width-0.5, 4.5);
+                ctx.moveTo(canvas.width - 0.5, 0);
+                ctx.lineTo(canvas.width - 0.5, 15);
+                ctx.moveTo(canvas.width - 0.5, 4.5);
                 ctx.lineTo(0, 4.5);
 
                 ctx.stroke();
@@ -168,12 +167,12 @@
                         ctx = me.get("ctx"),
                         labels = me.get("labelsEl"),
                         labelText, labelsHtml = "", offset;
-var width = me.get("gradient_width")||"256";
+                var width = me.get("gradient_width") || "256";
                 for (var i = 0; i < gradient.length; i++) {
 
-                    labelText = max * gradient[i].stop ;
-                    
-                    offset = (ctx.measureText(labelText).width / 2) ;
+                    labelText = max * gradient[i].stop;
+
+                    offset = (ctx.measureText(labelText).width / 2);
 
                     if (i === 0) {
                         offset = 0;
@@ -190,14 +189,17 @@ var width = me.get("gradient_width")||"256";
         if (options.legend) {
             var legendCfg = options.legend;
             legendCfg.gradient = options.gradient || {0.45: "rgb(0,0,255)", 0.55: "rgb(0,255,255)", 0.65: "rgb(0,255,0)", 0.95: "yellow", 1.0: "rgb(255,0,0)"};
-            var element = document.getElementById('leafmap');
+            var element;
+            if (legendCfg.element)
+                element = document.getElementById(legendCfg.element);
+            else
+                element = document.getElementById('leafmap');
             var legend = new legend(legendCfg);
             element.appendChild(legend.getElement());
 
         }
     },
-
-    _drawDebugInfo: function (ctx) {
+    _drawDebugInfo: function(ctx) {
         var canvas = L.DomUtil.create('canvas', 'leaflet-tile-debug');
         var tileSize = this.options.tileSize;
         canvas.width = tileSize;
@@ -218,13 +220,12 @@ var width = me.get("gradient_width")||"256";
         g.fillRect(max / 2 - 5, max / 2 - 5, 10, 10);
         g.strokeText(ctx.tilePoint.x + ' ' + ctx.tilePoint.y + ' ' + ctx.zoom, max / 2 - 30, max / 2 - 10);
 
-        this._drawPoint(ctx, [0,0]);
+        this._drawPoint(ctx, [0, 0]);
     },
-
     /*
      * Used for debug
      */
-    _drawPoint: function (ctx, geom) {
+    _drawPoint: function(ctx, geom) {
         var p = this._tilePoint(ctx, geom);
         var c = ctx.dbgcanvas;
         var g = c.getContext('2d');
@@ -235,8 +236,7 @@ var width = me.get("gradient_width")||"256";
         g.fill();
         g.restore();
     },
-
-    _createTile: function () {
+    _createTile: function() {
         var tile = L.DomUtil.create('div', 'leaflet-tile');
         tile.style.width = tile.style.height = this._getTileSize() + 'px';
         tile.galleryimg = 'no';
@@ -253,7 +253,6 @@ var width = me.get("gradient_width")||"256";
         }
         return tile;
     },
-
     /**
      * Inserts data into quadtree and redraws heatmap canvas
      */
@@ -278,11 +277,10 @@ var width = me.get("gradient_width")||"256";
         });
         this.redraw();
     },
-
     /**
      * Transforms coordinates to tile space
      */
-    _tilePoint: function (ctx, coords) {
+    _tilePoint: function(ctx, coords) {
         // start coords to tile 'space'
         var s = ctx.tilePoint.multiplyBy(this.options.tileSize);
 
@@ -294,7 +292,6 @@ var width = me.get("gradient_width")||"256";
         var y = Math.round(p.y - s.y);
         return [x, y];
     },
-
     /**
      * Creates a query for the quadtree from bounds
      */
@@ -302,53 +299,49 @@ var width = me.get("gradient_width")||"256";
         return {
             x: bounds.getSouthWest().lng,
             y: bounds.getSouthWest().lat,
-            width: bounds.getNorthEast().lng-bounds.getSouthWest().lng,
-            height: bounds.getNorthEast().lat-bounds.getSouthWest().lat
+            width: bounds.getNorthEast().lng - bounds.getSouthWest().lng,
+            height: bounds.getNorthEast().lat - bounds.getSouthWest().lat
         };
     },
-
-    _getLatRadius: function () {
+    _getLatRadius: function() {
         return (this.options.radius.value / 40075017) * 360;
     },
-
-    _getLngRadius: function (point) {
+    _getLngRadius: function(point) {
         return this._getLatRadius() / Math.cos(L.LatLng.DEG_TO_RAD * point.lat);
     },
-
     /*
      * The idea is to create two points and then get
      * the distance between the two in order to know what
      * the absolute radius in this tile could be.
      */
-    projectLatlngs: function (point) {
+    projectLatlngs: function(point) {
         var lngRadius = this._getLngRadius(point),
-            latlng2 = new L.LatLng(point.lat, point.lng - lngRadius, true),
-            p = this._map.latLngToLayerPoint(latlng2),
-            q = this._map.latLngToLayerPoint(point);
+                latlng2 = new L.LatLng(point.lat, point.lng - lngRadius, true),
+                p = this._map.latLngToLayerPoint(latlng2),
+                q = this._map.latLngToLayerPoint(point);
         return Math.max(Math.round(q.x - p.x), 1);
     },
-
-    _draw: function (ctx) {
+    _draw: function(ctx) {
         if (!this._quad || !this._map) {
             return;
         }
 
         var self = this,
-            options = this.options,
-            tile = ctx.canvas,
-            tileSize = options.tileSize,
-            radiusValue = this.options.radius.value;
+                options = this.options,
+                tile = ctx.canvas,
+                tileSize = options.tileSize,
+                radiusValue = this.options.radius.value;
 
         var localXY, value, pointsInTile = [];
 
         var nwPoint = ctx.tilePoint.multiplyBy(tileSize),
-            sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
+                sePoint = nwPoint.add(new L.Point(tileSize, tileSize));
 
         // Set the radius for the tile, if necessary.
         // The radius of a circle can be either absolute in pixels or in meters
         // The radius in pixels is not the same on the whole map.
         if (options.radius.absolute) {
-            var centerPoint = nwPoint.add(new L.Point(tileSize/2, tileSize/2));
+            var centerPoint = nwPoint.add(new L.Point(tileSize / 2, tileSize / 2));
             var p = this._map.unproject(centerPoint);
             radiusValue = this.projectLatlngs(p);
         }
@@ -357,7 +350,7 @@ var width = me.get("gradient_width")||"256";
             "radius": radiusValue,
             "element": tile,
             "visible": true,
-            "opacity": 100,  // we use leaflet's opacity for tiles
+            "opacity": 100, // we use leaflet's opacity for tiles
             "gradient": options.gradient,
             "debug": options.debug
         });
@@ -384,6 +377,6 @@ var width = me.get("gradient_width")||"256";
     }
 });
 
-L.TileLayer.heatMap = function (options) {
+L.TileLayer.heatMap = function(options) {
     return new L.TileLayer.HeatMap(options);
 };
